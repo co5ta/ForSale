71429d76fd42ae639a219d283edfd7e27baebd31
costa.monzili <costamonzili@icloud.com>
2022-06-25 20:32:52 +0200

    initial commit

---
+++ ForSale/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ForSale
+//
+//  Created by costa.monzili on 25/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
